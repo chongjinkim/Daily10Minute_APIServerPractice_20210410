1ed7561713655ea83255025faaad470d9a3b3b68
김종진 <martia0118@gmail.com>
2021-04-10 11:48:21 +0900

    서버에 로그인 하러 가는 함수 별도 생성  -ServerUtil 클래스에 따로 정리

---
+++ app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/utils/SeverUtil.kt
@@ -0,0 +1,24 @@
+package com.nepplus.daily10minute_apiserverpractice_20210410.utils
+
+class SeverUtil {
+
+    companion object{
+
+//        이 중괄호 안에 변수 / 함수는
+//        ServerUtil자체의 변수 or 함수()등으로 클래스 자체의 기능으로 활용 가능
+//        Static개념에 대응됨
+
+//        서버의 호스트 주소 저장.
+
+        val HOST_URL = "http://15.164.153.174"
+
+//        서버에 로그인을 요청하는 기능 => 함수로 만들고 사용하자
+
+        fun postRequestLogin(email : String, pw : String) {
+
+
+        }
+
+    }
+
+}
