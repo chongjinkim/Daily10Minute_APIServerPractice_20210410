d74093df88934a74f8dd0358ad6697dc8fa9cfa0
김종진 <martia0118@gmail.com>
2021-04-10 15:48:04 +0900

    자동로그인 여부 저장 / 조회 기능 추가

--- app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/ContextUtil.kt
+++ app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/ContextUtil.kt
@@ -1,9 +1,35 @@
 package com.nepplus.daily10minute_apiserverpractice_20210410
 
+import android.content.Context
+
 class ContextUtil {
 
     companion object{
 
+        private val prefName = "Daily10MinutesPref"
 
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+//        자동 로그인 설정여부 저장(Setter) 함수
+
+        fun setAutoLogin(context : Context, autoLogin : Boolean){
+
+//        메모장을 열어서 => 변수에 담아두자
+          val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+//         변수를 이용해서 => 실제 저장 여부 기로
+           pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+
+        }
+
+//        설정해둔 자동로그인 여부 확인(Getter) 함수
+
+
+        fun getAutoLogin(context: Context) : Boolean{
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getBoolean(AUTO_LOGIN, false)
+        }
     }
 }
