5440a8d89614b256fd34bccadc91a8a924fba2a1
김종진 <martia0118@gmail.com>
2021-04-10 11:09:38 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.daily10minute_apiserverpractice_20210410
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
