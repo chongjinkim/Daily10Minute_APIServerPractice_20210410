ef5a3957d142dc255c3244ba02311ba6fccdd94a
김종진 <martia0118@gmail.com>
2021-04-10 12:08:19 +0900

    로그인 API 호출 기능 작성

--- app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/utils/SeverUtil.kt
+++ app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/utils/SeverUtil.kt
@@ -1,5 +1,9 @@
 package com.nepplus.daily10minute_apiserverpractice_20210410.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class SeverUtil {
 
     companion object{
@@ -16,6 +20,33 @@ class SeverUtil {
 
         fun postRequestLogin(email : String, pw : String) {
 
+//            어느 주소로 가야하는가?/기능주소
+//
+
+            val urlString = "${HOST_URL}/user"
+
+
+//            갈때 어떤 파라미터를 가져가야 하는가? POST VS GET에 따라 다르다.
+//            post - formData
+
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .build()
+
+//            모든 정보 종합 + 어떤 메쏘드?
+
+            val request = Request.Builder()
+                .url("urlString")//어디로 가는지
+                .post(formData)//post방식 - 필요 데이터(formData)들고 가도록
+                .build()
+
+//            정리된 정보를 들고 => 실제 api 요청 진행
+
+//            클라이언트로써 동작하는 코드를 쉽게 작성하도록 도와주는 라이브러리 : OkHttp
+            val client = OkHttpClient()
+
+            client.newCall(request)
 
         }
 
