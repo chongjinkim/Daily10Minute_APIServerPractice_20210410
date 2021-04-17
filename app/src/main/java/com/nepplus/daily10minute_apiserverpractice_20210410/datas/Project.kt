c273e55d91b59bfb97bdc55ba9e81543261950ba
김종진 <martia0118@gmail.com>
2021-04-17 16:01:10 +0900

    내 최종 지원 상태에 따른 버튼 보여 주고 숨기기

--- app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/datas/Project.kt
+++ app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/datas/Project.kt
@@ -10,7 +10,8 @@ class Project (
         var imageUrl : String,
         var description : String,
         var onGoingUserCount : Int,
-        var proofMethod : String) : Serializable {
+        var proofMethod : String,
+        var myLastStatus : String?) : Serializable {
 
 //태그 목록을 저장하기 위한 ArrayList => 멤버변수 추가
 
@@ -18,7 +19,7 @@ class Project (
 
 // 보조 생성자 추가 => Project()만으로도 만들 수 있게
 
-    constructor() : this(0,"","","",0, "")
+    constructor() : this(0,"","","",0, "", null)
 
 //    기능을 추가 => json을 넣으면 (input) : Project로 변환(return) => 단순 기능. companion Ojbect이용
 
@@ -37,6 +38,15 @@ class Project (
 
           project.onGoingUserCount = jsonObj.getInt("ongoing_users_count")
           project.proofMethod = jsonObj.getString("proof_method")
+
+// 내 최종 도전 상태 => null일 가능성도 있다.
+// null인 데이터를 파싱하라고 하면 => 에러처리, 파싱 중단
+// null 인지 아닌지 확인 ? => null이 아닐때만 파싱하자
+         if(!jsonObj.isNull("my_last_status")){
+
+             project.myLastStatus = jsonObj.getString("my_last_status")
+
+         }
 
 //태그 목록  (JSONArray[ ]) => 반복 파싱 => tag에 String으로 추가.
 
