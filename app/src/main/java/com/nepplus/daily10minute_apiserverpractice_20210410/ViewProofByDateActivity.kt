c42ce3c23ce4784b698d13ca2512e301d129e894
김종진 <martia0118@gmail.com>
2021-04-24 10:20:12 +0900

    날짜 선택 팝업 출현

--- app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/ViewProofByDateActivity.kt
+++ app/src/main/java/com/nepplus/daily10minute_apiserverpractice_20210410/ViewProofByDateActivity.kt
@@ -1,7 +1,11 @@
 package com.nepplus.daily10minute_apiserverpractice_20210410
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_view_proof_by_date.*
 
 class ViewProofByDateActivity : BaseActivity() {
 
@@ -14,6 +18,29 @@ class ViewProofByDateActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        selectDateBtn.setOnClickListener {
+
+//            날짜가 선택되면 어떡할건지? 변수에 저장.
+
+            val dateSetListener = object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//선택된 년 / 월 / 일이 변수에 담겨져 있다. -> TextView에 반영을 해준다.
+
+                    Log.d("선택된날짜", "${year}년", "${month}월", "${dayOfMonth}일")
+
+                }
+
+
+            }
+
+//            실제로 달력 띄우기(alertDiaglog)
+
+            val datePickerDialog = DatePickerDialog(mContext, dateSetListener, 2021, 4, 24)
+
+             datePickerDialog.show()
+
+        }
     }
 
     override fun setValues() {
